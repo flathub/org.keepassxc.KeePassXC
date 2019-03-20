72f857827118f5e2ebe65e62f205657f76b355ef
AsavarTzeth <asavartzeth@gmail.com>
2019-03-20 23:01:04 +0100

    keepassxc: Update to 2.4.0
    Substitute with better explanation

--- command-wrapper.sh
+++ command-wrapper.sh
@@ -6,9 +6,13 @@
 
 # Respect user settings "UseCustomProxy=true/false", whether it works or not
 if [[ -S "/run/user/$UID/app/org.keepassxc.KeePassXC/kpxc_server" ]]; then
-  if [[ "$1" =~ 'chrome-extension://' ]]; then
+  if   [[ "$1" =~ 'chrome-extension://iopaggbpplllidnfmcghoonnokmjoicf/' ]]; then
+    keepassxc-proxy "$@"
+  elif [[ "$1" =~ 'chrome-extension://oboonakemofpalcgghocfoadofidjkkk/' ]]; then
     keepassxc-proxy "$@"
   elif [[ "$1" =~ '.mozilla/native-messaging-hosts/org.keepassxc.keepassxc_browser.json' ]]; then
+    keepassxc-proxy "$@"
+  elif [[ "$1" =~ '.tor-browser/app/Browser/TorBrowser/Data/Browser/.mozilla/native-messaging-hosts/org.keepassxc.keepassxc_browser.json' ]]; then
     keepassxc-proxy "$@"
   else
     keepassxc "$@"
